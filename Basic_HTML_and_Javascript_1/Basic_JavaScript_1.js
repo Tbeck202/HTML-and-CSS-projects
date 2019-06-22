4bbf2a4bc16ff987bc47a705155784c0723de0ec
Tbeck202 <52093735+Tbeck202@users.noreply.github.com>
2019-06-22 16:58:33 -0700

    Add Basic_HTML_and_Javascript_1 files via upload

---
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Take Note!");
+
+function My_First_Function() {
+		var string = "Kiss m, I'm Irish!";
+		var result = String.fontcolor("green")
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
